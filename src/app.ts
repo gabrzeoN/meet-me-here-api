3ba7b02955ef006eca220962df2a5781c7f1f655
Gabriel Cari <g.cari@hotmail.com>
2023-04-13 11:09:20 -0300

    feat(docs): added swagger

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+
 
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 import router from "./modules/index.js";
+// @ts-ignore
+import swaggerDoc from "./swagger.json" assert { type: "json" };
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(router);
-app.use(errorHandlerMiddleware);
+app
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+  .use(cors())
+  .use(express.json())
+  .use(router)
+  .use(errorHandlerMiddleware);
 
 export default app;
