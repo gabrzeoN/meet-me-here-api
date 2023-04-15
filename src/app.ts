bde326be9fcd2479492b2956d8dfb67e61736ff3
Gabriel Cari <g.cari@hotmail.com>
2023-04-15 15:15:26 -0300

    feat(users): listAllUsers

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,10 @@ import "express-async-errors";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 
-
-import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
-import router from "./modules/index.js";
 // @ts-ignore
 import swaggerDoc from "./swagger.json" assert { type: "json" };
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
+import router from "./modules/index.js";
 
 const app = express();
 app
